71acca7055abc792838a6540b98946580fdab84c
SpykSpartan <107502836+SpykSpartan@users.noreply.github.com>
2024-10-30 13:47:57 -0400

    begining of shader farm

--- CG_Assignment_1/Assets/Scripts/GamePlay/CubeMovement.cs
+++ CG_Assignment_1/Assets/Scripts/GamePlay/CubeMovement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CubeMovement : MonoBehaviour
 {
     public float rotationSpeed = 20.0f;
+    public float roationSpeed2 = 10.0f;
 
     void Update()
     {
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, roationSpeed2 * Time.deltaTime);
     }
 }   
